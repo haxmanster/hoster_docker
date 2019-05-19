24e555ead15c85566ad101d550dca3336439077a
haxmanster <grzegorz-wolyniec@o2.pl>
2019-05-19 21:11:34 +0200

    STARRT

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ubuntu:latest
+MAINTAINER Grzegorz-wolyniec
+LABEL version="1.0" location="India" type="centos-with-ssh"
+RUN apt-get update -y
+RUN apt-get install -y openssh* vim mc apache2 lsof sudo
+RUN service ssh start
+EXPOSE 22
+EXPOSE 80
+VOLUME ["/data"]
+COPY . /data
+RUN chmod +x /data/red.sh
+WORKDIR /root
+USER root
+ENTRYPOINT //data/red.sh && /bin/bash
---
+++ red.sh
@@ -0,0 +1,20 @@
+n=0;
+if [ n==0 ]
+ then
+  service ssh start
+  service apache2 start
+fi
+
+USER_NAME=user
+cat /etc/passwd | grep ${USER_NAME} >/dev/null 2>&1
+
+if [ $? -eq 0 ] ; then
+    echo "User Exists"
+else
+    useradd user -m
+    echo 'user:user1' | chpasswd
+    usermod -s /bin/bash user
+    usermod -aG sudo user
+    chown -R user:user /var/www/html
+fi
+exit 0
